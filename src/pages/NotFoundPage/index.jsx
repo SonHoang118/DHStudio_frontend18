80d6a979336d9c088ec5937619c9bc54f3b62477
SonHoang118 <130476375+SonHoang118@users.noreply.github.com>
2025-05-13 08:45:24 +0700

    Update index.jsx

--- src/pages/NotFoundPage/index.jsx
+++ src/pages/NotFoundPage/index.jsx
@@ -4,17 +4,6 @@ import img404 from '../../assets/images/404img.jpg'
 import { Link } from 'react-router-dom'
 
 const NotFoundPage = () => {
-
-  const [width, setWidth] = useState(window.innerWidth);
-
-  useEffect(() => {
-    const handleResize = () => setWidth(window.innerWidth);
-
-    window.addEventListener("resize", handleResize);
-    
-    // Cleanup khi component unmount
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
   return (
     <div className={styleModule.not_found_page}>
       <div className={styleModule.not_found_page_top}>
@@ -26,7 +15,6 @@ const NotFoundPage = () => {
         to={'/'}
         className={styleModule.not_found_page_btn}
       ><i className='fa-solid fa-house' /> Quay lại trang chủ</Link>
-      <p>Chiều rộng màn hình: {width}px</p>
     </div>
   )
 }
